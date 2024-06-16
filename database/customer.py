8bd4d7cf36e4c81e142d6580af3537e5e496e6ab
andre mendes <andre.mendes.sp01@gmail.com>
2024-06-16 17:21:06 -0300

    Alterando a varivel da tabela  para , e modificar o template list_customer.html para ser exibida na home principal

--- database/customer.py
+++ database/customer.py
@@ -1,10 +1,10 @@
 CUSTOMER = [
-    {"id": 1 ,"nome": "Guilherme" , "email": "teste@teste.com"},
-    {"id": 2 ,"nome": "Jorge" , "email": "ssdf@teste.com"},
-    {"id": 3 ,"nome": "Maria" , "email": "rtete@teste.com"},
-    {"id": 4 ,"nome": "Flavia" , "email": "gsdfgd@teste.com"},
-    {"id": 5 ,"nome": "João" , "email": "gsgf@teste.com"},
-    {"id": 6 ,"nome": "Paulo" , "email": "tesgfgste@teste.com"},
-    {"id": 7 ,"nome": "Python" , "email": "sgfgsg@teste.com"},
+    {"id": 1 ,"name": "Guilherme" , "email": "teste@teste.com"},
+    {"id": 2 ,"name": "Jorge" , "email": "ssdf@teste.com"},
+    {"id": 3 ,"name": "Maria" , "email": "rtete@teste.com"},
+    {"id": 4 ,"name": "Flavia" , "email": "gsdfgd@teste.com"},
+    {"id": 5 ,"name": "João" , "email": "gsgf@teste.com"},
+    {"id": 6 ,"name": "Paulo" , "email": "tesgfgste@teste.com"},
+    {"id": 7 ,"name": "Python" , "email": "sgfgsg@teste.com"},
 ]
 
